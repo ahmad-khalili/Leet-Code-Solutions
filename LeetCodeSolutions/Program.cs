b2d8624c5dc28b45ca8d4d906343777ee2288d1d
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 17:46:45 +0300

    Add Roman to Integer problem's solution

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace LeetCodeSolutions;
+public class Program
+{
+    public int toRoman(char letter)
+    {
+        switch (letter)
+        {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+    
+    public int RomanToInt(string str)
+    {
+        int result = 0;
+        
+        foreach (var letter in str)
+        {
+            result += toRoman(letter);
+        }
 
-Console.WriteLine("Hello, World!");
+        if (str.Contains("IV") || str.Contains("IX"))
+        {
+            result -= 2;
+        }
+
+        if (str.Contains("XL") || str.Contains("XC"))
+        {
+            result -= 20;
+        }
+
+        if (str.Contains("CD") || str.Contains("CM"))
+        {
+            result -= 200;
+        }
+
+        return result;
+    }
+
+    public static void Main()
+    {
+        Program program = new Program();
+        Console.WriteLine(program.RomanToInt("MCMXCIV"));
+    }
+}
