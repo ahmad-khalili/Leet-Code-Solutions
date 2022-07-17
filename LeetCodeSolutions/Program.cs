6c8d71156b1014771d3817dfb8995e3f4acdc3a9
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-17 17:48:58 +0300

    Add Valid Parentheses problem's solution

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace LeetCodeSolutions;
+public class Program
+{
+    public bool IsValid(string s) {
+        Stack<char> strStack = new Stack<char>();
+        Dictionary<char, char> parentheses = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+        foreach(var p in s){
+            if (parentheses.ContainsKey(p)) strStack.Push(p);
+            if (p.Equals(')') || p.Equals('}') || p.Equals(']')){
+                if (strStack.Count > 0){
+                    char top = strStack.Peek();
+                    if(p.Equals(parentheses[top])){
+                        strStack.Pop();
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        if (strStack.Count == 0) return true;
+        
+        return false;
+    }
+    public static void Main()
+    {
+        Program program = new Program();
+        var testParentheses = "()[]{}";
+        Console.WriteLine(program.IsValid(testParentheses));
+    }
+}
