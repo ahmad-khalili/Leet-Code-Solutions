53e75204e463b061f78baaa2a30def647892ad8f
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-06 13:28:16 +0300

    Add a dictionary for Roman letters

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,6 +1,16 @@
 ﻿namespace LeetCodeSolutions;
 public class Program
 {
+    private readonly Dictionary<string, int> _romanLetters = new Dictionary<string, int>()
+    {
+        {"I", 1},
+        {"V", 5},
+        {"X", 10},
+        {"L", 50},
+        {"C", 100},
+        {"D", 500},
+        {"M", 1000}
+    };
     public int toRoman(char letter)
     {
         switch (letter)
